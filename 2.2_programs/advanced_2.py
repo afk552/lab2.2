58ba86bf302a89187f4e0302cb4a2cbeb3c90de3
afk552 <ez552@mail.ru>
2022-12-02 23:48:03 +0300

    Update advanced

--- 2.2_programs/advanced_2.py
+++ 2.2_programs/advanced_2.py
@@ -10,27 +10,16 @@ EULER = 0.5772156649015328606
 EPS = 1e-10
 
 if __name__ == '__main__':
-    x = int(input("Enter x: "))
-    n = 2
-    # Первый член ряда включен в результат
-    result = -(x ** n / (n * math.factorial(n)))
-    # Переменная отображающая смену знака
-    shuffle = True
-    while True:
-        if x == 0:
-            print("Illegal value of x", file=sys.stderr)
-            exit(1)
-        else:
-            if not shuffle:
-                n += 2
-                member = x ** n / (n * math.factorial(n))
-                result += -member
-                shuffle = True
-            else:
-                n += 2
-                member = x ** n / (n * math.factorial(n))
-                result += member
-                shuffle = False
-            if math.fabs(member) < EPS:
-                break
-    print(result + EULER + math.log(x))
+    x = float(input("Введите x: "))
+    if x == 0:
+        print("Неверно задан X", file=sys.stderr)
+        exit(1)
+    a = x
+    S, n = a, 1
+    # Сумма членов ряда
+    while math.fabs(a) > EPS:
+        a *= -1*x**2*(2*n)/(2*n+2)**2
+        S += a
+        n += 1
+    # Значение функции
+    print(f"Сi({x}) = {EULER + math.log(math.fabs(x)) + S}")
