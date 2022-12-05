d1e1b3503dae1c6e283dada0080e4ffacc8268aa
afk552 <ez552@mail.ru>
2022-12-05 23:15:51 +0300

    Fix indiv 1

--- 2.2_programs/task1_indiv_11.py
+++ 2.2_programs/task1_indiv_11.py
@@ -4,10 +4,13 @@
 if __name__ == '__main__':
     n = int(input("Enter kWh: "))
     cost = 0
+    k1 = 7
+    k2 = 17
+    k3 = 20
     if n <= 250:
-        cost = n * 7
-    elif 250 < n <= 300:
-        cost = n * 17
-    elif n > 300:
-        cost = n * 20
+        cost = n * k1
+    elif n <= 300:
+        cost = k1*250+(n-250)*k2
+    else:
+        cost = k1*250+k2*300+(n-300)*k3
     print("The final cost is:", cost)
